21031173f8601894cbebd57cf548e0934ade02c7
Arne <arne@rnae.de>
2023-02-04 12:21:48 +0100

    feat: personality saving works

--- usermods/rdm_dmx/usermod_rdm_dmx.h
+++ usermods/rdm_dmx/usermod_rdm_dmx.h
@@ -10,10 +10,17 @@
 
 void startAddressChangedCb(uint16_t newAddr);
 void dmxTask(void *instance);
-
 class RdmDmx;
-// poor mans singleton, but this is good enough for now
-RdmDmx *rdmDmx = nullptr;
+
+struct DmxTaskParams
+{
+    RdmDmx *rdmDmx;
+    uint16_t numPixels;
+    std::string defaultPersonalityName;
+    uint16_t defaultPersonalityFootprint;
+    std::atomic<uint8_t> personality = 0;
+
+} dmxParams; // This is global because the esp_dmx api uses c-callbacks and I cannot capture state in the callback lambdas
 
 class RdmDmx : public Usermod
 {
@@ -22,11 +29,19 @@ public:
     int dmxAddr;
     std::atomic<int> newDmxAddr;
     std::atomic<bool> identify = false;
+
+    /** personality:
+     *  0 = wled fixture
+     *  1 = pixel mapping no pixels grouped
+     *  2 = pixel mapping 2 pixels grouped
+     *  3 = pixel mapping 3 pixels grouped
+     *  etc..
+     */
     uint8_t dmxData[13] = {0}; // FIXME hardcoded 13
 
     void setup()
     {
-        rdmDmx = this;
+
         // FIXME use correct upper bound
         if (dmxAddr < 1 || dmxAddr > 512)
         {
@@ -35,7 +50,14 @@ public:
         }
         newDmxAddr = dmxAddr;
 
+        dmxParams.rdmDmx = this;
+        dmxParams.numPixels = strip.getLengthTotal();
+        // the default personality will get id 1.
+        dmxParams.defaultPersonalityName = "WLED Mode";
+        dmxParams.defaultPersonalityFootprint = 13;
+
         TaskHandle_t dmxTaskHandle = NULL;
+
         // pin to core 0 because wled is running on core 1
         xTaskCreatePinnedToCore(dmxTask, "DMX_TASK", 10240, nullptr, 2, &dmxTaskHandle, 0);
         if (!dmxTaskHandle)
@@ -53,6 +75,14 @@ public:
             serializeConfig();
         }
 
+        dmxParams.numPixels = strip.getLengthTotal();
+        if (dmxParams.numPixels != strip.getLengthTotal())
+        {
+            // TODO
+            // num pixels has changed, re-calculate personalities
+            ESP_LOGE("RdmDmx", "num pixels has changed. please restart to calculate correct personalities");
+        }
+
         updateEffect();
     }
 
@@ -64,6 +94,11 @@ public:
 
     void updateEffect()
     {
+
+        // setPixelColor
+        // show
+        //  auf strip
+
         if (identify)
         {
             bri = 255;
@@ -88,7 +123,7 @@ public:
             }
             if (dmxData[1] < strip.getModeCount())
                 effectCurrent = dmxData[1];
-            effectSpeed = dmxData[2]; 
+            effectSpeed = dmxData[2];
             effectIntensity = dmxData[3];
             effectPalette = dmxData[4];
             col[0] = dmxData[5];
@@ -109,6 +144,7 @@ public:
     {
         JsonObject top = root.createNestedObject("dmx_rdm_mod");
         top["dmx_addr"] = dmxAddr;
+        top["personality"] = dmxParams.personality.load();
     }
 
     void updateDmxAddr(uint16_t newAddr)
@@ -130,6 +166,10 @@ public:
         JsonObject top = root["dmx_rdm_mod"];
         bool configComplete = !top.isNull();
         configComplete &= getJsonValue(top["dmx_addr"], dmxAddr, 42);
+        uint8_t person = 0;
+        configComplete &= getJsonValue(top["personality"], person, 0);
+        dmxParams.personality = person;
+        rdm_client_set_personality(DMX_NUM_2, person + 1); //+1 because rmd is 1-based
         return configComplete;
     }
 
@@ -137,7 +177,37 @@ public:
     {
         return USERMOD_ID_RDM_DMX;
     }
+
+    // callback coming from rdm client
+    void personalityChanged(uint8_t newPersonality)
+    {
+        // rmd personalities are 1-based but we are 0-based
+        dmxParams.personality = newPersonality - 1;
+        serializeConfig();
+    }
 };
+
+void initPersonalities()
+{
+    // generate pixel mapping personalities
+    for (int i = 1; i <= 20; ++i)
+    {
+        const uint16_t divisor = i;
+        const uint16_t numPixelsGrouped = (dmxParams.numPixels + divisor - 1) / divisor; // integer math ceil
+        const uint16_t footprint = numPixelsGrouped * 3;                                 // FIXME support RGBW?
+        if (footprint > 512)
+        {
+            ESP_LOGE(TAG, "dmx error: Too many pixels for footprint %d. Skipping...", divisor);
+            continue;
+        }
+        rdm_client_add_personality(DMX_NUM_2, footprint, ("Pixmap (grp " + std::to_string(divisor) + ")").c_str());
+    }
+
+    rdm_client_set_personality(DMX_NUM_2, dmxParams.personality + 1); //+1 because rmd is 1-based
+
+    rdm_client_set_personality_changed_cb(DMX_NUM_2, [](uint8_t personality)
+                                          { dmxParams.rdmDmx->personalityChanged(personality); });
+}
 
 void dmxTask(void *)
 {
@@ -148,13 +218,15 @@ void dmxTask(void *)
     uint8_t data[DMX_MAX_PACKET_SIZE] = {0};
     ESP_ERROR_CHECK(dmx_set_pin(DMX_NUM_2, TX_PIN, RX_PIN, EN_PIN));
     ESP_ERROR_CHECK(dmx_driver_install(DMX_NUM_2, DMX_DEFAULT_INTR_FLAGS));
-
     // TODO use SP_ERROR_CHECK
-    rdm_client_init(DMX_NUM_2, rdmDmx->dmxAddr, 13, "LICHTAUSGANG Blinder");
+    rdm_client_init(DMX_NUM_2, dmxParams.rdmDmx->dmxAddr, dmxParams.defaultPersonalityFootprint, "LICHTAUSGANG Blinder", dmxParams.defaultPersonalityName.c_str());
+
     rdm_client_set_start_address_changed_cb(DMX_NUM_2, [](uint16_t newAddr)
-                                            { rdmDmx->updateDmxAddr(newAddr); });
+                                            { dmxParams.rdmDmx->updateDmxAddr(newAddr); });
     rdm_client_set_notify_cb(DMX_NUM_2, [](bool identify)
-                             { rdmDmx->setIdentify(identify); });
+                             { dmxParams.rdmDmx->setIdentify(identify); });
+
+    initPersonalities();
 
     while (true)
     {
@@ -173,12 +245,12 @@ void dmxTask(void *)
                 else
                 {
                     // FIXME only read part of the buffer directly into rdmDmx?!
-                    rdmDmx->newDmxData(data);
+                    dmxParams.rdmDmx->newDmxData(data);
                 }
             }
             else
             {
-                ESP_LOGE(TAG, "dmx error: %s", esp_err_to_name(dmxPacket.err));
+                ESP_LOGE("RdmDmx", "dmx error: %s", esp_err_to_name(dmxPacket.err));
             }
         }
     }
